7a8c6516b26c0a01d9febaaea99d702ae35222d5
shuaibkhan_unitedsol <shuaib.khan@unitedsol.net>
2025-09-10 15:01:32 +0500

    ..

---
+++ assets/js/data.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const products = [
+  {
+    id: 1,
+    badge: "19% OFF",
+    img: "../assets/images/prod_6.png",
+    title: "Prestashop Buy from External Link Module",
+    rating: "4.4",
+    downloads: "24.6k",
+    date: "Sept 2024",
+    price: "€49",
+    link: "#"
+  },
+  {
+    id: 2,
+    badge: "19% OFF",
+    img: "../assets/images/prod_6.png",
+    title: "Prestashop Advanced SEO Optimizer",
+    rating: "4.7",
+    downloads: "18.2k",
+    date: "Aug 2024",
+    price: "€59",
+    link: "#"
+  },
+  {
+    id: 3,
+    badge: "25% OFF",
+    img: "../assets/images/prod_6.png",
+    title: "Prestashop Social Login Module",
+    rating: "4.8",
+    downloads: "32.1k",
+    date: "July 2024",
+    price: "€69",
+    link: "#"
+  },
+];
