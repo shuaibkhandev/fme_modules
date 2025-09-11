9efea79f79b009f4ffd130ee901ebc057f4e9748
shuaibkhan_unitedsol <shuaib.khan@unitedsol.net>
2025-09-11 10:24:50 +0500

    ..

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,18 +10,32 @@ top_list_item.forEach((item) => {
 
 
     // Testimonails Swiper SLider
-        var swiper = new Swiper(".testimonials_swiper", {
-      slidesPerView: 3,
-      spaceBetween: 30,
-        navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+var swiper = new Swiper(".testimonials_swiper", {
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+ 0: {
+  slidesPerView:1,
+    spaceBetween: 10,
+ },
+ 576: {
+  slidesPerView:2,
+  spaceBetween:15,
+ },
+ 992:{
+  slidesPerView:3,
+  spaceBetween:32
+ }
+  },
+});
+
 
 
 // Search
